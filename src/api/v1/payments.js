2539e6f0065694322a56114e8e9c32f5c9d26d72
jnlmendonca <jnl.mendonca@gmail.com>
2019-09-03 23:36:56 +0100

    Solve tests

--- src/api/v1/payments.js
+++ src/api/v1/payments.js
@@ -2,6 +2,23 @@ const models = require('../../data')
 const errors = require('../../config/errors')
 
 
+function _validateId(id, format=/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/) {
+    if (!id || !format.test(id)) {
+        return false
+    }
+
+    return true
+}
+
+function _equalIds(idA, idB) {
+    if (idA && idB && idA === idB) {
+        return true
+    }
+
+    return false
+}
+
+
 function listPayments (req, res, next) {
 
     let offset = parseInt(req.query.offset) || 0
@@ -19,7 +36,7 @@ function listPayments (req, res, next) {
         limit = 50
     }
 
-    models.Payment.find({}, {createdAt: 0, updatedAt: 0, __v: 0}, {skip: offset, limit: limit, sort: {createdAt: 1}}, (err, payments) => {
+    models.Payment.find({}, {createdAt: 0, updatedAt: 0, __v: 0, 'attributes._id': 0, 'attributes.createdAt': 0, 'attributes.updatedAt': 0, 'attributes.__v': 0}, {skip: offset, limit: limit, sort: {createdAt: 1}}, (err, payments) => {
         return res.status(200).json(payments)
     })
 }
@@ -28,13 +45,11 @@ function listPayments (req, res, next) {
 function retrievePayment (req, res, next) {
 
     // Validate payment id
-    const idFormat = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
-
-    if (!req.params.id || !idFormat.test(req.params.id)) {
+    if (!_validateId(req.params.id)) {
         return res.status(400).send(errors.InvalidIdError)
     }
 
-    models.Payment.findOne({_id: req.params.id}, {createdAt: 0, updatedAt: 0, __v: 0}, (err, payment) => {
+    models.Payment.findOne({_id: req.params.id}, {createdAt: 0, updatedAt: 0, __v: 0, 'attributes._id': 0, 'attributes.createdAt': 0, 'attributes.updatedAt': 0, 'attributes.__v': 0}, (err, payment) => {
         if (err || !payment) {
             return res.status(404).send()
         }
@@ -45,6 +60,35 @@ function retrievePayment (req, res, next) {
 
 
 function createPayment (req, res, next) {
+
+    // Validate ids
+    if (req.body && req.body.attributes) {
+
+        if (!_validateId(req.body.attributes.beneficiary_party_id)) {
+            return res.status(400).send(errors.InvalidDataError)
+        }
+
+        if (!_validateId(req.body.attributes.sponsor_party_id)) {
+            return res.status(400).send(errors.InvalidDataError)
+        }
+
+        if (!_validateId(req.body.attributes.debtor_party_id)) {
+            return res.status(400).send(errors.InvalidDataError)
+        }
+
+        if (_equalIds(req.body.attributes.debtor_party_id, req.body.attributes.sponsor_party_id)) {
+            return res.status(400).send(errors.BusinessRulePartyConflictError)
+        }
+
+        if (_equalIds(req.body.attributes.debtor_party_id, req.body.attributes.beneficiary_party_id)) {
+            return res.status(400).send(errors.BusinessRulePartyConflictError)
+        }
+
+        if (_equalIds(req.body.attributes.sponsor_party_id, req.body.attributes.beneficiary_party_id)) {
+            return res.status(400).send(errors.BusinessRulePartyConflictError)
+        }
+
+    }
 
     models.Payment.create(req.body, (err, payment) => {
         if (err || !payment) {
@@ -59,9 +103,7 @@ function createPayment (req, res, next) {
 function deletePayment (req, res, next) {
 
     // Validate payment id
-    const idFormat = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
-
-    if (!req.params.id || !idFormat.test(req.params.id)) {
+    if (!_validateId(req.params.id)) {
         return res.status(400).send(errors.InvalidIdError)
     }
 
@@ -78,13 +120,11 @@ function deletePayment (req, res, next) {
 function updatePayment (req, res, next) {
 
     // Validate payment id
-    const idFormat = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
-
-    if (!req.params.id || !idFormat.test(req.params.id)) {
+    if (!_validateId(req.params.id)) {
         return res.status(400).send(errors.InvalidIdError)
     }
 
-    models.Payment.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, useFindAndModify: false, fields: {createdAt: 0, updatedAt: 0, __v: 0}}, (err, payment) => {
+    models.Payment.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, useFindAndModify: false, fields: {createdAt: 0, updatedAt: 0, __v: 0, 'attributes._id': 0, 'attributes.createdAt': 0, 'attributes.updatedAt': 0, 'attributes.__v': 0}}, (err, payment) => {
         if (err) {
             return res.status(400).json(errors.InvalidDataError)
         }
